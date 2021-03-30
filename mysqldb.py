81701ba5fd07d094fde504b7936b2cf8d9a67052
rgengyx <rickygeng97@gmail.com>
2021-03-30 22:09:50 +0800

    commit

--- mysqldb.py
+++ mysqldb.py
@@ -3,5 +3,5 @@ import pymysql
 # Database Connection
 conn = pymysql.connect(host='localhost',
                        user='root',
-                       password='12qwaszx',
+                       password='12qwaszX!',
                        db='amazon_review')
